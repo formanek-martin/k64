77520efa0838cb96d346249d3da7e500b35598ab
Martin Formánek <formanek.martin@seznam.cz>
2023-01-31 19:19:01 +0100

    --

--- k64-dir/react_app/src/components/App.js
+++ k64-dir/react_app/src/components/App.js
@@ -1,9 +1,30 @@
+import {useEffect} from "react";
+import useFetch from "../helpers/useFetch.js"
+
 export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello world!</p>
-      </header>
-    </div>
-  );
+    const testFetch = useFetch("https://v6.exchangerate-api.com/v6/6a8f3dbacea5f5c5a792ef7c/");
+    const jsonAPI = useFetch("http://localhost/k64/k64-dir/web/jsonapi/");
+
+
+    // useEffect(() => {
+    //     testFetch.get("latest/USD")
+    //     .then(data => {
+    //         console.log(data.conversion_rates.CZK);
+    //     });
+    // }, []);
+    
+    useEffect(() => {
+        jsonAPI.get("node/testovaci")
+        .then(data => {
+            console.log(data);
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Hello world!</p>
+            </header>
+        </div>
+    );
 }
