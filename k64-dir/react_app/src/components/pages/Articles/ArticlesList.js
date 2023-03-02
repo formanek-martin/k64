9766e772ae903f9078a4c7bc8e0be02e36f78695
Martin Formánek <formanek.martin@seznam.cz>
2023-03-03 00:13:00 +0100

    listing articles

--- k64-dir/react_app/src/components/pages/Articles/ArticlesList.js
+++ k64-dir/react_app/src/components/pages/Articles/ArticlesList.js
@@ -1,25 +1,35 @@
-import {useEffect} from "react";
+import {useState, useEffect} from "react";
+import {Link} from "react-router-dom";
 import useFetch from "../../../helpers/useFetch.js"
 
 export default function ArticlesList() {
     const jsonAPI = useFetch("jsonapi/");
+    const [articles, setArticles] = useState([]);
+    console.log("rendered ArticlesList");
     
     useEffect(() => {
-        jsonAPI.get("node/article")
+        jsonAPI.get("node/article?page[limit]=5&sort=-created&fields[node--article]=field_description,title,path")
         .then(data => {
-            console.log(data);
+            console.log("used effect");
+            setArticles(data.data);
         });
     }, []);
-    
-    // TODO: article list
     
     return (
         <div className="articles_list k-grid">
             <div className="k-grid--center">  
                 <h1>výpis článků</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec condimentum leo. Cras eu tempus eros, quis ullamcorper massa. Sed blandit arcu in ante aliquet eleifend. Suspendisse malesuada risus vitae libero porttitor congue. Sed ultricies ex sed fermentum porta. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras maximus tempor orci, ut euismod mauris consectetur vitae. Sed iaculis nulla sapien, quis malesuada orci posuere non. Suspendisse id tellus in lacus ullamcorper venenatis sed viverra ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam laoreet eget purus at dictum. Nunc venenatis consequat tellus, id tincidunt augue euismod ac. Curabitur neque orci, faucibus non hendrerit luctus, finibus in sem. Maecenas nec fermentum nulla. Nunc lacinia nisl eu purus pulvinar ullamcorper ut nec tellus.</p>
-                <p>Etiam suscipit posuere tristique. Curabitur diam lorem, malesuada sit amet mauris sed, vestibulum condimentum est. Praesent tincidunt mauris eget feugiat sagittis. Duis metus nulla, eleifend eu sem sagittis, convallis mattis mauris. Integer ac faucibus orci, ut ullamcorper lectus. Sed nec blandit dui. Suspendisse commodo lacus quis tellus venenatis, ut fermentum elit eleifend. Morbi suscipit elit ac felis volutpat, a malesuada odio gravida.</p>
-                <p>Maecenas vitae tristique quam, vitae dictum est. Cras at nisl id mi imperdiet fringilla et vel arcu. Maecenas et lorem at quam pellentesque consectetur sit amet a ligula. Donec a nunc fringilla, mollis neque et, mattis lacus. Proin in scelerisque nulla, blandit euismod ligula. Suspendisse blandit neque ac orci imperdiet ornare. Mauris luctus mi vitae nunc consectetur cursus. Suspendisse ullamcorper magna ut urna pretium pellentesque. Nunc feugiat, sem eu iaculis efficitur, tellus sem bibendum nisl, et consectetur odio nunc vitae neque. Aliquam ut nisi augue. Sed blandit, neque eu vehicula posuere, velit nunc rutrum ipsum, id cursus justo velit a eros.</p>
+                <div className="articles_list-content">
+                    {articles.map(article => {
+                        return (
+                            <div className="article_push" key={article.id}>
+                                <h2>{article.attributes.title}</h2>
+                                <p>{article.attributes.field_description}</p>
+                                <Link to={article.attributes.path.alias}>přečíst článek</Link>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
