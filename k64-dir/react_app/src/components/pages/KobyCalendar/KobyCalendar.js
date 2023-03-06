054d74f022fc292095795a44581c30546df9dc0a
Martin Formánek <formanek.martin@seznam.cz>
2023-03-06 22:47:46 +0100

    kalendar fetch data

--- k64-dir/react_app/src/components/pages/KobyCalendar/KobyCalendar.js
+++ k64-dir/react_app/src/components/pages/KobyCalendar/KobyCalendar.js
@@ -1,23 +1,55 @@
-import moment from 'moment';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
+import {Calendar, dayjsLocalizer} from 'react-big-calendar';
+import {useState, useEffect} from "react";
+import {Helmet} from 'react-helmet';
+import dayjs from 'dayjs';
+import useFetch from "../../../helpers/useFetch.js";
+import Loader from "../../elements/Loader/Loader.js";
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { Helmet } from 'react-helmet';
+
+require('dayjs/locale/cs');
+dayjs.locale('cs');
+const localizer = dayjsLocalizer(dayjs);
 
 export default function KobyCalendar() {
-    const localizer = momentLocalizer(moment);
+    const {get} = useFetch("view-udalosti");
+    const [isLoading, setIsLoading] = useState(false);
+    const [eventsList, setEventsList] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        get("")
+        .then(data => {
+            const tempList = [];
+            data.forEach(event => {
+                event.field_cas_udalosti.forEach(eventDate => {
+                    tempList.push({
+                        title: event.title,
+                        start: new Date(eventDate * 1000),
+                        end: new Date((eventDate  * 1000) + (2*60*60*1000)) //TODO: replace with real value from JSON or leave with default +2h
+                    });
+                });
+            });
+            setEventsList(tempList);
+        })
+        .finally(() => {
+            setIsLoading(false);
+        });
+    }, []);
+
     return (
         <div className="k-grid">
             <Helmet>
                 <title>Šachy Kobylisy | Kalendář</title>
             </Helmet>
             <div className="k-grid--center">
-                <Calendar
-                    localizer={localizer}
-                    // events={myEventsList}
-                    startAccessor="start"
-                    endAccessor="end"
-                    style={{ height: 500 }}
-                />
+                {isLoading ? <Loader /> : <Calendar
+                        localizer={localizer}
+                        events={eventsList}
+                        startAccessor="start"
+                        endAccessor="end"
+                        style={{ height: 500 }}
+                    />
+                }
             </div>
         </div>
     )
