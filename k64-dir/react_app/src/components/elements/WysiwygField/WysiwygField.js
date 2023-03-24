249f3e74de849838534703235732323207325d15
Martin Formánek <formanek.martin@seznam.cz>
2023-03-24 01:08:36 +0100

    adding PGN viewer

--- k64-dir/react_app/src/components/elements/WysiwygField/WysiwygField.js
+++ k64-dir/react_app/src/components/elements/WysiwygField/WysiwygField.js
@@ -1,23 +1,36 @@
+import { useEffect } from 'react';
 import parse from 'html-react-parser';
 import { updateWysiwygField } from "../../../helpers/helpers";
-// import LichessPgnViewer from 'lichess-pgn-viewer';
-
+import { pgnView } from '@mliebelt/pgn-viewer';
+import './WysiwygField.css';
 
 export default function WysiwygField(props) {
+    const id_el = "something";
+
     let content = props.children;
     content = updateWysiwygField(content);
     
+    // TODO make it possible to have more boards or more games in one PGN
+    useEffect(() => {
+        const id = "board";
+        const id_sel = document.querySelector("#" + id);
+        const once_class = "k64-chessboard";
+       
+        if (id_sel && !id_sel.classList.contains(once_class)) {
+            id_sel.classList.add(once_class);
+            // console.log(id_sel);
+            const chessboard_width = id_sel.offsetWidth > 600 ? "600px" : id_sel.offsetWidth + "px";
 
-    // const lpv = LichessPgnViewer(domElement, {
-    //     pgn: 'e4 c5 Nf3 d6 e5 Nc6 exd6 Qxd6 Nc3 Nf6',
-    // });
-      
-    // // lpv is an instance of Ctrl, providing some utilities such as:
-    // lpv.goTo('first');
-    // lpv.goTo('next');
-    // lpv.flip();
-    // console.log(lpv.game);
-
+            pgnView("board", {
+                pgn: id_sel.innerText,
+                showResult: true,
+                boardSize: chessboard_width,
+                pieceStyle: 'merida',
+                figurine: true
+            });
+        }
+    });
+    
     return (
         <div className="k64_wysiwyg">
             {parse(content)}
