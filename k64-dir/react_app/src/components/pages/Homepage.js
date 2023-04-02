f39cb5484f90e717927c707ca5e0d339ae18503f
Martin Formánek <formanek.martin@seznam.cz>
2023-04-02 21:15:14 +0200

    HomePopup

--- k64-dir/react_app/src/components/pages/Homepage.js
+++ k64-dir/react_app/src/components/pages/Homepage.js
@@ -1,12 +1,15 @@
+
 import WordCloud from "../elements/WordCloud/WordCloud.js";
 import { Helmet } from 'react-helmet-async';
 import './Homepage.css';
+import HomePopup from "../elements/HomePopup/HomePopup.js";
 
 export default function Homepage() {
     return (<>
             <Helmet>
                 <title>Šachy Kobylisy | Homepage</title>
             </Helmet>
+            <HomePopup />
             <WordCloud />
         </>
     );
