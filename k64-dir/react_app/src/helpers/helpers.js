6c1a8cb200e680b9fce4a42387ffc2f01d57feb2
Martin Formánek <formanek.martin@seznam.cz>
2023-04-02 12:15:03 +0200

    debug image replacing

--- k64-dir/react_app/src/helpers/helpers.js
+++ k64-dir/react_app/src/helpers/helpers.js
@@ -2,10 +2,11 @@ import { path_DEV, path_CMS } from "./envVariables";
 
 // adding PNG library to make magic done
 export function updateWysiwygField(text) {
-    
+    console.log("path_DEV: " + path_DEV);
+    console.log("path_CMS: " + path_CMS);
     // replacing relative image paths to absolute and target them to CMS endpoint
     const text__fixed_images = text.replaceAll(path_DEV, path_CMS);
-    const text__wrapper = `<div>${text__fixed_images}</div>`;
+    const text__wrapper = `${text__fixed_images}`;
 
     return text__wrapper;
 }
